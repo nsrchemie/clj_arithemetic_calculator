2ae4bc00b608ec71c4eee891087a543f2a6819b4
nsrchemie <nsrchemie@gmail.com>
2016-08-18 15:42:16 -0400

    converted both integers, hardcorded addition works,also changed output format

--- calc.clj
+++ calc.clj
@@ -1,5 +1,5 @@
 (defn calc [x]
 	(def nums (take-nth 2 x))
 	(def num1 (Character/digit (first nums) 10))
-	(def num2 (last nums))
-	(println num1, (type num1), (type num2)))
+	(def num2 (Character/digit (last nums) 10))
+	(pr-str (+ num1 num2)))
