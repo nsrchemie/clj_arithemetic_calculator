0e7a7ad545654f8904ebb7b1fb256c9ed5cec7e9
nsrchemie <nsrchemie@gmail.com>
2016-08-18 17:59:10 -0400

    removed number variables, moved them to output line

--- calc.clj
+++ calc.clj
@@ -1,7 +1,6 @@
 (defn calc [x]
 	(def nums (take-nth 2 x))
-	(def num1 (Character/digit (first nums) 10))
-	(def num2 (Character/digit (last nums) 10))
 	(def ops {"+" +})
 	(def oper (str (get x 1)))
-	(pr-str ((ops oper) num1 num2)))
+	(pr-str ((ops oper) (Character/digit (first nums) 10)
+	 (Character/digit (last nums) 10))))
