2c3f417cfcd4d663794496a8cc78c1743a614d8c
nsrchemie <nsrchemie@gmail.com>
2016-08-18 19:10:46 -0400

    added function to evaluate numbers without having to call them individually

--- calc.clj
+++ calc.clj
@@ -1,6 +1,7 @@
 (defn calc [x]
 	(def nums (take-nth 2 x))
 	(def corrected (for [x nums] (Character/digit x 10)))
-	(def ops {"+" +})
+	(def ops {"+" + "-" - "/" / "*" *})
 	(def oper (str (get x 1)))
-	(pr-str ((ops oper) (first corrected) (last corrected))))
+	(defn combine [vals] (reduce (ops oper) vals))
+	(pr-str (combine corrected)))
