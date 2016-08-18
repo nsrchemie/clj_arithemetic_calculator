4c6642271252e960d1d361ba9aa6867b45f3c299
nsrchemie <nsrchemie@gmail.com>
2016-08-18 17:55:17 -0400

    created operator map, successfully using it to dynamically evaluate input

--- calc.clj
+++ calc.clj
@@ -2,4 +2,5 @@
 	(def nums (take-nth 2 x))
 	(def num1 (Character/digit (first nums) 10))
 	(def num2 (Character/digit (last nums) 10))
-	(pr-str (+ num1 num2)))
+	(def ops {"+" +})
+	(pr-str ((ops (str (get x 1))) num1 num2)))
